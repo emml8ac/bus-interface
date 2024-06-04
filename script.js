5cdd9963de9f4c694029aa1f15310a66fa3b21b6
“[Emmanuel8a]” <gudalvxd@gmail.com>
2024-06-03 21:12:58 -0500

    Se añadio logica del boton de modo oscuro

--- script.js
+++ script.js
@@ -60,3 +60,17 @@ function removeElement(array, element) {
     }
     return array;
 }
+const temaOscuro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark")
+    document.querySelector("body").setAttribute("style", "transition:200ms")
+    document.querySelector("#icon").setAttribute("class", "bi bi-sun-fill")
+}
+const temaClaro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light")
+    document.querySelector("body").setAttribute("style", "transition:200ms")
+    document.querySelector("#icon").setAttribute("class", "bi bi-moon-fill")
+}
+const cambiarTema = () =>{
+    document.querySelector("body").getAttribute("data-bs-theme") ==="light"?
+    temaOscuro():temaClaro()
+}
